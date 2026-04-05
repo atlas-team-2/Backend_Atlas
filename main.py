af9fc5ce3e25b8477fd0e24d0f293b5ffe9acd4a
asilya18 <asilasakirova@gmail.com>
2026-04-05 20:54:10 +0300

    Remove custom OpenAPI

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from pathlib import Path
-
-import yaml
 from fastapi import FastAPI
 
 app = FastAPI(
@@ -16,15 +14,3 @@ SPEC_PATH = Path(__file__).parent / 'openapi_atlas.yaml'
 def read_root():
     return {'message': 'Atlas Naroda API is running'}
 
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-
-    with open(SPEC_PATH, 'r', encoding='utf-8') as file:
-        app.openapi_schema = yaml.safe_load(file)
-
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
