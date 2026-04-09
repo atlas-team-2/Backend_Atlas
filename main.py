9ee0981caa75d08e692dbed0f950111446b02a18
AzalinaF <faiazalina.2019@gmail.com>
2026-04-09 18:28:12 +0300

    feat: connect db

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from app.core.config import settings
+from app.db import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+
 app = FastAPI(
-    title='Atlas Naroda API',
-    version='1.0.0',
-    description='API для проекта Атлас народа',
+    title=settings.app_name,
+    version=settings.app_version,
+    description=settings.app_description,
+    lifespan=lifespan,
 )
 
 
 @app.get('/')
-def read_root():
+async def read_root():
     return {'message': 'Atlas Naroda API is running'}
 
 
