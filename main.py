582e8d4ffb7d396f11663aad2cfdcf56315696c6
AzalinaF <faiazalina.2019@gmail.com>
2026-04-15 22:31:59 +0300

    refactor: move db config to database module

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.core.config import settings
-from app.db import create_db_and_tables
+from app.db.database import create_db_and_tables
 
 
 @asynccontextmanager
