8c6d2349ba18a4fee73347429196c02c9b031450
asilya18 <asilasakirova@gmail.com>
2026-04-23 21:30:59 +0300

    fix: fixed core/settings

--- app/core/db_config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class DBConfig(BaseSettings):
-    schema: str = 'postgresql+asyncpg'
-    host: str = 'localhost'
-    user: str = 'postgres'
-    password: str = 'postgres'
-    port: int = 5432
-    name: str = 'atlas_db'
