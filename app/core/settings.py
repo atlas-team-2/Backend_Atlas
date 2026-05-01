ab9f65856c66d97b7a50dd1a747d41981a9bd2d4
Азалина <faiazalina.2019@gmail.com>
2026-05-01 20:18:27 +0300

    Task authentication (#36)

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from functools import lru_cache
 
 from pydantic import SecretStr
@@ -13,12 +14,12 @@ class AppSettings(BaseSettings):
 class AuthSettings(BaseSettings):
     secret: SecretStr = 'change_me_to_openssl_secret_at_least_32_chars'
     algorithm: str = 'HS256'
-    access_token_lifetime_seconds: int = 300
-    refresh_token_lifetime_seconds: int = 3600
+    access_token_lifetime: timedelta = timedelta(hours=1)
+    refresh_token_lifetime: timedelta = timedelta(days=1)
 
 
 class DbSettings(BaseSettings):
-    drivername: str = 'postgresql+asyncpg'
+    schema: str = 'postgresql+asyncpg'
     host: str = 'localhost'
     user: str = 'postgres'
     password: str = 'postgres'
